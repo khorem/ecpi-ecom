330dee0c81aac24f0efc6637fc23496a882f11c3
filipcisco <Ndiogoye.Cisco@outlook.com>
2023-12-20 13:51:35 +0000

    Project initialization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {UsersPageComponent} from "./pages/users-page/users-page.component";
+import {ProductsPageComponent} from "./pages/products-page/products-page.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
+import {CategoriesComponent} from "./pages/components/categories/categories.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: LandingPageComponent },
+  { path: 'users', component: UsersPageComponent },
+  { path: 'products', component: ProductsPageComponent },
+  { path: 'category/:id', component: CategoriesComponent },
+  { path: '', redirectTo: 'products', pathMatch:  'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ src/app/pages/user-detail-page/user-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-detail-page.component.html',
+  styleUrl: './user-detail-page.component.scss'
+})
+export class UserDetailPageComponent {
+
+}
